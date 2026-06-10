9d6943f3801753952e95b77f89de6583bbb353a0
Pradeep <lampinthesea224@gmail.com>
2026-06-11 01:36:27 +0530

    Java Basics: variables and data types

---
+++ 01-Basics/Variables.java
@@ -0,0 +1,17 @@
+// package 01-Basics;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age = 20;
+        double cgpa = 7.97;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Pradeep";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
