49b4493f063e6be4554e317aa35ee67d120d270d
Pradeep <lampinthesea224@gmail.com>
2026-06-15 21:34:34 +0530

    Array: array length function

---
+++ 02-Arrays/arrayOutput.java
@@ -0,0 +1,8 @@
+public class arrayOutput {
+    public static void main(String[] args) {
+        int[] arr = {22, 33, 44, 55, 66, 77, 88, 99, 110, 112, 117};
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
