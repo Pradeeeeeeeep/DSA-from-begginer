29c30c1f256ff4804b1e654ec468eb828ef1f3a3
Pradeep <lampinthesea224@gmail.com>
2026-06-18 15:33:39 +0530

    Array: traverse of string

---
+++ 03-Strings/traverseStr.java
@@ -0,0 +1,8 @@
+public class traverseStr {
+    public static void main(String[] args) {
+        String name = "Pradeeeeeep";
+        for(int i=0; i<name.length(); i++){
+            System.out.println(name.charAt(i));
+        }
+    }
+}
