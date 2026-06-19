bca4e97c106555b3023f873e7a0bb5a5f273f35e
Pradeep <lampinthesea224@gmail.com>
2026-06-19 20:52:50 +0530

    Recursion: Fibonachi Number

--- 04-Recursion/fibonachi.java
+++ 04-Recursion/fibonachi.java
@@ -1,3 +1,12 @@
 public class fibonachi {
-    
+    static int nfibonachi(int n){
+        if (n<=1) {
+            return n;
+        }
+        return nfibonachi(n-1) + nfibonachi(n-2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(nfibonachi(6));
+    }
 }
